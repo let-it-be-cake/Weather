0c8d9774242bde11f5883f3c2dda6b9deb78b78c
let-it-be-cake <44724939+let-it-be-cake@users.noreply.github.com>
2018-11-08 16:24:13 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,37 @@
+var weather = {
+  /*country: '',
+  temp: '',
+  clouds: ''*/
+}
+var url = 'https://api.openweathermap.org/data/2.5/weather?q='+ 'Moscow' +',' + 'ru' +'&appid=96df07f28842d4e02eaa7cb3535fd5f2';
+
+var xhr = new XMLHttpRequest();
+
+function query(theUrl, theWeather) {
+  xhr.open('GET', theUrl);
+  xhr.addEventListener('readystatechange', function () {
+    if (xhr.readyState != xhr.DONE) { 
+    	return; 
+    }
+    if (xhr.status != 200) {
+      console.log('Failed to get data from server. Check your request and try again');
+    } else {
+      var answer = JSON.parse(xhr.responseText);
+      theWeather.country = answer.name;
+      theWeather.temp = answer.main.temp;
+      theWeather.clouds = answer.clouds.all;
+      console.log(theWeather);
+
+      	//need to finish
+		document.querySelectorAll('p')[0].textContent += theWeather.country;
+		document.querySelectorAll('p')[1].textContent += (theWeather.temp - 273);
+		document.querySelectorAll('p')[2].textContent += theWeather.clouds;
+
+
+      return theWeather;
+    }
+  })
+  xhr.send();
+}
+
+weather = query(url, weather);
